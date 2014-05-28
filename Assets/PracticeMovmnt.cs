060c7a36799df69c18bf09992decc7b26a859086
schilling117 <schilling.117@osu.edu>
2014-05-27 20:35:36 -0400

    5/27/14
    map created, and scaling corrected

---
+++ Assets/PracticeMovmnt.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PracticeMovmnt : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	public float movementSpeed = 10;
+	public float turningSpeed = 60;
+	// Update is called once per frame
+		
+	void Update() {
+		float horizontal = Input.GetAxis("Horizontal") * turningSpeed * Time.deltaTime;
+		transform.Rotate(0, horizontal, 0);
+
+		float vertical = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
+		transform.Translate(0, 0, vertical);
+	}
+}
+
+
--- Assets/movement.cs
+++ Assets/movement.cs
@@ -6,11 +6,17 @@ public class movement : MonoBehaviour {
 	// Use this for initialization
 	public int speed =  5;
 	public double dodgeTime = 2.0;
+	public double attackTime = 0.2;
+	public int attackSpeed = 40;
 	public int dodgeSpeed = 20;
 	public int sprintSpeed = 10;
 	public int walkSpeed = 5;
 	public double startDodge;
+	public double startAttack;
 	public bool dodged = false;
+	public bool attacking = false;
+	public bool movingForward = false;
+	public bool debugging = false;
 
 	void Start () {
 
@@ -29,11 +35,15 @@ public class movement : MonoBehaviour {
 			}
 		}
 		if(Input.GetButton ("Vertical")){
+			// backwards
 			if(Input.GetAxis("Vertical")<0){
 				transform.Translate (new Vector3(0,0,-1) * Time.deltaTime*speed);
+				movingForward = false;
 			}
+			// forwards
 			else{
 				transform.Translate (new Vector3(0,0,1) * Time.deltaTime*speed);
+				movingForward = true;
 			}
 		}
 		if(Input.GetButtonDown("Jump")){
@@ -49,23 +59,41 @@ public class movement : MonoBehaviour {
 		if(Input.GetButtonUp ("Sprint")){
 			speed = walkSpeed;
 		}
-
-		Debug.Log(speed);
+		if (debugging) {
+			Debug.Log (speed);
+		}
 	
-		if(Input.GetButtonDown ("Dodge")){
-			dodgeTime = 0.5;
+		if(Input.GetButtonDown ("Dodge") && !dodged){
+			//dodgeTime = 0.5; // may be problem
 			startDodge = Time.time;
 			speed = dodgeSpeed;
 			dodged = true;
 		}
-		Debug.Log (startDodge);
-		double endDodge = startDodge + dodgeTime;
-		Debug.Log ("Projected stop: " + endDodge);
-		Debug.Log ("Dodgetime: " + dodgeTime);
+		if (debugging) {
+			Debug.Log (startDodge);
+			double endDodge = startDodge + dodgeTime;
+			Debug.Log ("Projected stop: " + endDodge);
+			Debug.Log ("Dodgetime: " + dodgeTime);
+		}
 		if (Time.time > startDodge + dodgeTime && dodged) {
 			Debug.Log ("Finish time: " + Time.time); 
 			speed = walkSpeed;
 			dodged = false;
+		}
+		if (Input.GetButtonDown ("Attack") && !attacking) {
+			startAttack = Time.time;
+
+			if(movingForward)
+			{
+			speed = attackSpeed;
+			}
+
+			attacking= true;
+		}
+		if (Time.time > startAttack + attackTime && attacking) {
+			speed = walkSpeed;
+			attacking = false;
+
 		}
 
 	}
